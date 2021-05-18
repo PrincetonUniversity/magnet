8ec065dd4d028cc9dc7b36d133a26e08c453b594
MInjie Chen <minjie@ieee.org>
2021-05-18 01:31:49 -0400

    Update Procfile
    fixed the extension for procfile

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"minjie@ieee.org\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
