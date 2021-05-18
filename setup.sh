ff14aab3380342da719aa46878259f4614022338
MInjie Chen <minjie@ieee.org>
2021-05-18 02:38:51 -0400

    Update setup.sh
    Go back to original

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 mkdir -p ~/.streamlit/
-echo "[theme]\n\
-primaryColor = ‘#84a3a7’\n\
-backgroundColor = ‘#EFEDE8’\n\
-secondaryBackgroundColor = ‘#fafafa’\n\
-textColor= ‘#424242’\n\
-font = ‘sans serif’\n\
+echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
