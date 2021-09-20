9050536d245028a5970434444b5b20f5363481f4
Vineet Bansal <vineetbansal@protonmail.com>
2021-09-20 16:29:31 -0400

    core_loss function signature cleanup

--- src/pmagnet/core.py
+++ src/pmagnet/core.py
@@ -2,21 +2,21 @@ import numpy as np
 from pmagnet.constants import materials
 
 
-def core_loss_iGSE(freq, flux_delta, flux, duty, k_i, alpha, beta, n_interval=10001):
+def core_loss_iGSE(freq, flux, duty, k_i, alpha, beta, n_interval=10001):
     """
     Calculate magnetic core loss using iGSE
 
-    :param freq: frequency of excitation waveform
-    :param flux_delta:
-    :param flux:
-    :param duty:
-    :param k_i:
-    :param alpha:
-    :param beta:
-    :param n_interval:
-    :return:
+    :param freq: Frequency of excitation waveform (Hz)
+    :param flux: Relative Flux Density (mT) in a single waveform cycle, as an ndarray
+    :param duty: Duty (%) in a single waveform cycle, as an ndarray
+    :param k_i: Stenmetz coefficient k_i
+    :param alpha: Steinmetz coefficient alpha
+    :param beta: Steinmetz coefficient beta
+    :param n_interval: No. of intervals to use to solve iGSE using trapezoidal rule
+    :return: Core loss (kW/m^3)
     """
     period = 1 / freq
+    flux_delta = np.amax(flux) - np.amin(flux)
     time, dt = np.linspace(start=0, stop=period, num=n_interval, retstep=True)
     B = np.interp(time, np.multiply(duty, period), flux)
     dBdt = np.gradient(B, dt)
@@ -25,19 +25,21 @@ def core_loss_iGSE(freq, flux_delta, flux, duty, k_i, alpha, beta, n_interval=10
     return core_loss
 
 
-def core_loss(material, freq, flux_delta, flux, duty, algorithm='iGSE'):
+def core_loss(material, freq, flux, duty, algorithm='iGSE'):
     """
     Calculate magnetic core loss of a material
 
     :param material: One of N27/N87/N49
-    :param freq: Frequency of excitation waveform
-    :param flux_delta:
-    :param flux: Waveform Pattern Relative Flux Density (mT)
-    :param duty: Waveform Pattern Duty in a Cycle (%)
+    :param freq: Frequency of excitation waveform (Hz)
+    :param flux: Relative Flux Density (mT) in a single waveform cycle, as an ndarray
+    :param duty: Duty (%) in a single waveform cycle, as an ndarray
     :param algorithm: Only 'iGSE' supported for now
-    :return:
+    :return: Core loss (kW/m^3)
     """
     assert material in materials, f'Material {material} not found'
     k_i, alpha, beta = materials[material]
-    core_loss_fn = {'iGSE': core_loss_iGSE}[algorithm]
-    return core_loss_fn(freq, flux_delta, flux, duty, k_i, alpha, beta)
+    core_loss_fn = {
+        'iGSE': core_loss_iGSE
+    }[algorithm]
+
+    return core_loss_fn(freq, flux, duty, k_i, alpha, beta)
--- tests/test_coreloss.py
+++ tests/test_coreloss.py
@@ -8,7 +8,7 @@ def test_coreloss1():
     flux = np.array([-4.54545455, 5.45454545, 15.45454545, 5.45454545, 15.45454545, 25.45454545, -14.54545455,
                      -34.54545455, 5.45454545, -14.54545455, -4.54545455])
     duty = np.array([0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0])
-    loss = core_loss_iGSE(10_000, 60, flux, duty, k_i=4.88e-10, alpha=1.09, beta=2.44)
+    loss = core_loss_iGSE(10_000, flux, duty, k_i=4.88e-10, alpha=1.09, beta=2.44)
 
     assert loss == approx(0.002811408865609484)
 
@@ -17,6 +17,6 @@ def test_coreloss2():
     rng = default_rng(515242)
     flux = 100 * rng.random(11)
     duty = np.array([0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0])
-    loss = core_loss_iGSE(10_000, 60, flux, duty, k_i=4.88e-10, alpha=1.09, beta=2.44)
+    loss = core_loss_iGSE(10_000, flux, duty, k_i=4.88e-10, alpha=1.09, beta=2.44)
 
-    assert loss == approx(0.02183420474055214)
+    assert loss == approx(0.030457525717187316)
