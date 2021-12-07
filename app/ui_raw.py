5f94562d96033c46ba9a891d356fbda6bcef0696
Vineet Bansal <vineetbansal@protonmail.com>
2021-12-07 18:00:16 -0500

    Getting raw data file path from magnet.ini

--- app/ui_raw.py
+++ app/ui_raw.py
@@ -1,17 +1,16 @@
-import re
-import numpy as np
+import os.path
 import streamlit as st
-from magnet.constants import input_dir
-
 
 from magnet import config
-from magnet.constants import material_names  # , excitations # We don't need "Datasheet"
+from magnet.constants import material_names, input_dir
+
 
 def header(material, excitation):
     s = f'Download Raw Data - {material} Material {excitation} '
     return st.header(s)
 
-def ui_download_raw_data(m):
+
+def ui_download_raw_data(m, streamlit_root):
     st.sidebar.header(f'Information for Material {m}')
     material = st.sidebar.selectbox(f'Material {m}:', material_names)
     excitation = st.sidebar.selectbox(f'Excitation {m}:', ("Sinusoidal", "Triangular-Trapezoidal"))
@@ -19,8 +18,9 @@ def ui_download_raw_data(m):
     # It does not make sense to have "Datasheet", also, Triangular and Trapezoidal are saved into the same zip file
     header(material, excitation)
 
-    file_zip = input_dir + material + "_" + excitation + "_Raw_Data_Short.zip"
-    st.download_button(f'Download ZIP file', open(file_zip, "rb"), material + "_" + excitation + ".zip", "zip", key=m)
+    data_file = os.path.join(streamlit_root, config.data.raw_data_file.format(material=material, excitation=excitation))
+    with open(data_file, 'rb') as file:
+        st.download_button(f'Download Data file', file, os.path.basename(data_file), key=m)
 
     file_txt = input_dir + material + "_" + excitation + "_Test_Info.txt"
     txt_info_file = open(file_txt)
