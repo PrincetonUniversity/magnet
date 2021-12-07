b6aac157245763f6d95147fa5c609b1185903486
Diego Serrano <91206350+ds9056@users.noreply.github.com>
2021-12-07 17:51:11 -0500

    Data displayed in its correct units
    Now using importlib.resources
    dataframe extended with freq_kHz, flux_mT and loss_kW/m3
    Some errors fixed

--- app/ui_raw.py
+++ app/ui_raw.py
@@ -1,15 +1,16 @@
 import re
 import numpy as np
 import streamlit as st
-from magnet.constants import input_dir
-
+from importlib.resources import path
 
 from magnet import config
 from magnet.constants import material_names  # , excitations # We don't need "Datasheet"
 
+
 def header(material, excitation):
     s = f'Download Raw Data - {material} Material {excitation} '
     return st.header(s)
+
 
 def ui_download_raw_data(m):
     st.sidebar.header(f'Information for Material {m}')
@@ -19,13 +20,14 @@ def ui_download_raw_data(m):
     # It does not make sense to have "Datasheet", also, Triangular and Trapezoidal are saved into the same zip file
     header(material, excitation)
 
-    file_zip = input_dir + material + "_" + excitation + "_Raw_Data_Short.zip"
-    st.download_button(f'Download ZIP file', open(file_zip, "rb"), material + "_" + excitation + ".zip", "zip", key=m)
+    with path('magnet.data', f'{material}_{excitation}_Raw_Data_Short.zip') as file_zip:
 
-    file_txt = input_dir + material + "_" + excitation + "_Test_Info.txt"
-    txt_info_file = open(file_txt)
-    for line in txt_info_file:
-        st.write(line)
+        st.download_button(f'Download ZIP file', open(file_zip, "rb"), material + "_" + excitation + ".zip", "zip", key=m)
+
+    with path('magnet.data', f'{material}_{excitation}"_Test_Info.txt') as file_txt:
+        txt_info_file = open(file_txt)
+        for line in txt_info_file:
+            st.write(line)
 
     st.sidebar.markdown("""---""")
     st.markdown("""---""")
