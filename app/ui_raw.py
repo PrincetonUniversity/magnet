c3186946352c0ef44bd07cb328827f0d6fe26502
Diego Serrano <91206350+ds9056@users.noreply.github.com>
2021-12-29 17:33:08 -0500

    Adding the single cycle zip to the download function
    The download page now has two download buttons, one for the raw files, the other for the B-H files with 100 samples.
    It worked when I changed the path to my local one, and I hope it works with the files in the server too, but I don't know how to check that before committing.

--- app/ui_raw.py
+++ app/ui_raw.py
@@ -4,11 +4,12 @@ from magnet import config
 from magnet.constants import material_names
 from magnet.io import load_metadata
 
+
 def header(material, excitation):
-    s = f'Download Raw Data - {material} - {excitation} '
+    s = f'Download Data - {material} - {excitation} '
     return st.header(s)
 
-  
+
 def ui_download_raw_data(m, streamlit_root):
     st.sidebar.header(f'Information for Material {m}')
     material = st.sidebar.selectbox(f'Material {m}:', material_names)
@@ -23,18 +24,28 @@ def ui_download_raw_data(m, streamlit_root):
 
     header(material, excitation)
 
-    data_file = os.path.join(streamlit_root, config.streamlit.raw_data_file.format(material=material, excitation=read_excitation))
-    with open(data_file, 'rb') as file:
-        st.download_button(f'Download Data file', file, os.path.basename(data_file), key=m)
-
     metadata = load_metadata(material, read_excitation)
-    st.write(metadata['info_date'])
-    st.write(metadata['info_excitation'])
-    st.write(metadata['info_core'])
-    st.write(metadata['info_setup'])
-    st.write(metadata['info_scope'])
-    st.write(metadata['info_volt_meas'])
-    st.write(metadata['info_curr_meas'])
+    with st.expander("Measurement details"):
+        st.write(metadata['info_date'])
+        st.write(metadata['info_excitation'])
+        st.write(metadata['info_core'])
+        st.write(metadata['info_setup'])
+        st.write(metadata['info_scope'])
+        st.write(metadata['info_volt_meas'])
+        st.write(metadata['info_curr_meas'])
+
+    st.write('Raw data - 20us sample')
+    data_file_raw = os.path.join(streamlit_root,
+                            config.streamlit.raw_data_file.format(material=material, excitation=read_excitation))
+    with open(data_file_raw, 'rb') as file:
+        st.download_button(f'Download zip file', file, os.path.basename(data_file_raw), key=[m, 'Raw'])
+
+    st.write('Post-processed data - a single cycle')
+
+    data_file_cycle = os.path.join(streamlit_root,
+                             config.streamlit.single_cycle_file.format(material=material, excitation=read_excitation))
+    with open(data_file_cycle, 'rb') as file:
+        st.download_button(f'Download zip file', file, os.path.basename(data_file_cycle), key=[m, 'Cycle'])
 
     st.sidebar.markdown("""---""")
     st.markdown("""---""")
--- src/magnet/core.py
+++ src/magnet/core.py
@@ -10,7 +10,6 @@ def default_units(prop):  # Probably we are not going to need the default units
     return {
         'frequency': 'Hz',
         'flux_density': 'T',
-        # 'duty_ratio': '',
         'power_loss': '[W/m^3]',
         'outlier_factor': '%'
     }[prop]
@@ -134,19 +133,19 @@ def core_loss_iGSE_trapezoid(freq, flux, duty_ratios, k_i=None, alpha=None, beta
     assert np.all((0 <= np.array(duty_ratios)) & (np.array(duty_ratios) <= 1)), 'Duty ratios should be between 0 and 1'
 
     frac_time = np.array([0, duty_ratios[0], duty_ratios[0]+duty_ratios[2], 1-duty_ratios[2], 1])
-    if duty_ratios[0]>duty_ratios[1] :
-        BPplot=flux # Since Bpk is proportional to the voltage, and the voltage is proportional to (1-dp+dN) times the dp
-        BNplot=-BPplot*((-1-duty_ratios[0]+duty_ratios[1])*duty_ratios[1])/((1-duty_ratios[0]+duty_ratios[1])*duty_ratios[0]) # proportional to (-1-dp+dN)*dn
-    else :
-        BNplot=flux # proportional to (-1-dP+dN)*dN
-        BPplot=-BNplot*((1-duty_ratios[0]+duty_ratios[1])*duty_ratios[0])/((-1-duty_ratios[0]+duty_ratios[1])*duty_ratios[1]) # proportional to (1-dP+dN)*dP
+    if duty_ratios[0]>duty_ratios[1]:
+        BPplot = flux  # Since Bpk is proportional to the voltage, and the voltage is proportional to (1-dp+dN) times the dp
+        BNplot = -BPplot*((-1-duty_ratios[0]+duty_ratios[1])*duty_ratios[1])/((1-duty_ratios[0]+duty_ratios[1])*duty_ratios[0]) # proportional to (-1-dp+dN)*dn
+    else:
+        BNplot = flux  # proportional to (-1-dP+dN)*dN
+        BPplot = -BNplot*((1-duty_ratios[0]+duty_ratios[1])*duty_ratios[0])/((-1-duty_ratios[0]+duty_ratios[1])*duty_ratios[1]) # proportional to (1-dP+dN)*dP
     flux_list = dc_bias + np.array([-BPplot,BPplot,BNplot,-BNplot,-BPplot])
     
     return core_loss_iGSE_arbitrary(freq, flux_list, frac_time, k_i=k_i, alpha=alpha, beta=beta, material=material)
 
 
 def core_loss_ML_trapezoid(freq, flux, duty_ratios, material):
-    nn = model(material=material,waveform='Trapezoidal')
+    nn = model(material=material, waveform='Trapezoidal')
     core_loss = 10.0 ** nn(
         torch.from_numpy(
             np.array([
@@ -166,7 +165,7 @@ def core_loss_ML_arbitrary(material, freq, flux_list, frac_time):
     nn = model_lstm(material=material)
     Ts = 10e-9
     Num = 5000
-    time = np.linspace(start = Ts, stop = Ts*Num, num = Num)
+    time = np.linspace(start=Ts, stop=Ts*Num, num=Num)
     period = 1/freq
     flux = np.interp(np.remainder(time,period),np.multiply(frac_time,period),flux_list)
     flux = torch.from_numpy(flux).view(-1, 5000, 1)
@@ -178,7 +177,7 @@ def loss(waveform, algorithm, **kwargs):
     if algorithm == 'Machine Learning':
         algorithm = 'ML'
     assert waveform in ('sine', 'triangle', 'trapezoid', 'arbitrary'), f'Unknown waveform {waveform}'
-    assert algorithm in ('iGSE', 'ML' ,'Ref'), f'Unknown algorithm {algorithm}'
+    assert algorithm in ('iGSE', 'ML', 'Ref'), f'Unknown algorithm {algorithm}'
 
     fn = globals()[f'core_loss_{algorithm}_{waveform}']
     return fn(**kwargs)
