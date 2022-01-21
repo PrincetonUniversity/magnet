079f6eaef8be9278f6843e6a4515d2f2302a316c
ds9056 <91206350+ds9056@users.noreply.github.com>
2022-01-21 15:21:41 +0000

    Deploying to gh-pages from @ PrincetonUniversity/magnet@3ce28e908b39c00fcd52b1a483a79ccfc59be61f 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g1ab74aa',
+    VERSION: '0.1.dev1+g3ce28e9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
