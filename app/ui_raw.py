87a822a18a4015ca209c5bc3cd8b8536cace91ee
Diego Serrano <91206350+ds9056@users.noreply.github.com>
2022-01-21 22:14:12 -0500

    Description of the raw data added
    A couple of images added to clarify the concepts.
    Description shown before material A to avoid repetition

--- app/ui_raw.py
+++ app/ui_raw.py
@@ -1,12 +1,49 @@
 import os.path
+from PIL import Image
 import streamlit as st
-
 from magnet import config as c
 from magnet.constants import material_names, material_manufacturers, excitations_raw
 from magnet.io import load_metadata
 
+STREAMLIT_ROOT = os.path.dirname(__file__)
+
 
 def ui_download_raw_data(m, streamlit_root):
+
+    if m == 'A':
+        col1, col2 = st.columns(2)
+        with col1:
+            st.subheader('Voltage and current data from the oscilloscope ')
+            st.write("""
+                       This .zip file contains a .txt file with information regarding the setup and core under test and two .csv files, one for the current and the other for the voltage.
+                       These files contain the samples measured from the oscilloscope.
+        
+                       Each row is a different measurement (i.e. a data point with a different frequency, flux density, etc.)
+                       and each column is a sample; there are 2.000 samples per data-point.
+                       The sampling time is 10 ns, so the total waveform last 20 us.
+        
+                       We are working to add a .cvs file with the commanded duty cycles and frequency of each data point.
+                   """)
+        with col2:
+            st.subheader('Single switching cycle post-processed B-H data')
+            st.write("""
+                   This .zip file contains 4 files.
+    
+                   A .txt file with information regarding the setup, core under test, and post-processing information.
+    
+                   A .cvs file for the B waveform, where each row is a data point, and each column is a sample.
+                   100 samples are saved per data point.
+    
+                   A .csv file for the H waveform, with the same structure as the B file.
+    
+                   Finally, a .csv file contains the information of the sampling time, where each row corresponds to the data-point of the B and H files.
+               """)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(Image.open(os.path.join(STREAMLIT_ROOT, 'img', 'download_raw.png')), width=500)
+        with col2:
+            st.image(Image.open(os.path.join(STREAMLIT_ROOT, 'img', 'download_single.png')), width=500)
+
     st.sidebar.header(f'Information: Case {m}')
     excitation = st.sidebar.selectbox(
         f'Excitation:',
@@ -46,16 +83,6 @@ def ui_download_raw_data(m, streamlit_root):
                 c.streamlit.raw_data_file.format(material=material, excitation=read_excitation))
             with open(data_file_raw, 'rb') as file:
                 st.download_button(f'Download zip file', file, os.path.basename(data_file_raw), key=[m, 'Raw'])
-            st.write("""
-                This .zip file contains a .txt file with information regarding the setup and core under test and two .csv files, one for the current and the other for the voltage.
-                These files contain the samples measured from the oscilloscope.
-
-                Each row is a different measurement (i.e. a data point with a different frequency, flux density, etc.)
-                and each column is a sample; there are 2.000 samples per data-point.
-                The sampling time is 10 ns, so the total waveform last 20 us.
-
-                We are working to add a .cvs file with the commanded duty cycles and frequency of each data point.
-            """)
         else:
             st.write('Download data missing, please contact us')
 
@@ -67,18 +94,6 @@ def ui_download_raw_data(m, streamlit_root):
                 streamlit_root, c.streamlit.single_cycle_file.format(material=material, excitation=read_excitation))
             with open(data_file_cycle, 'rb') as file:
                 st.download_button(f'Download zip file', file, os.path.basename(data_file_cycle), key=[m, 'Cycle'])
-            st.write("""
-                This .zip file contains 4 files.
-        
-                A .txt file with information regarding the setup, core under test, and post-processing information.
-        
-                A .cvs file for the B waveform, where each row is a data point, and each column is a sample.
-                100 samples are saved per data point.
-        
-                A .csv file for the H waveform, with the same structure as the B file.
-        
-                Finally, a .csv file contains the information of the switching frequency, where each row corresponds to the data-point of the B and H files.
-            """)
         else:
             st.write('Download data missing, please contact us')
 
