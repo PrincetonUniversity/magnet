c0bb6d0de3bd77fe3e2c39d30340bdb4b6fc5d72
MInjie Chen <minjie@ieee.org>
2022-11-09 16:50:58 -0500

    Update ui_intro.py
    New update

--- app/ui_intro.py
+++ app/ui_intro.py
@@ -17,8 +17,8 @@ def convert_df(df):
 
 def ui_intro(m):
     st.title('MagNet AI for Research, Education and Design')
-    st.markdown('"Are you still using the Steinmetz Equation (1892) in 2022?" - MagNet AI')
-    st.caption('We created MagNet AI to advance power magnetics research, education, and design. The mission of MagNet AI is to replace the traditional curve-fitting models (e.g., Steinmetz Equations and Jiles-Atherton Models) with state-of-the-art data-driven methods such as neural networks and machine learning. MagNet AI is open, transparent, fast, smart, versatile, and is continously getting better. It is a new tool to design power magnetics and can do lots of things that traditional methods cannnot do.')
+    st.markdown('"Are you still using the Steinmetz Equation proposed in 1890 to design power magnetics in 2022?" - try MagNet AI')
+    st.caption('We created MagNet AI to advance power magnetics research, education, and design. The mission of MagNet AI is to replace the traditional curve-fitting models (e.g., Steinmetz Equations and Jiles-Atherton Models) with state-of-the-art data-driven methods such as neural networks and machine learning. MagNet AI is open, transparent, fast, smart, versatile, and is continously learning. It is a new tool to design power magnetics and can do lots of things that traditional methods cannnot do.')
     st.markdown("""---""")
     
     col1, col2 = st.columns(2)
